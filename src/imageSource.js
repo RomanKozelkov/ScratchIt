a83954b310d591c629c13596453df30d50334d5e
Roman Kozelkov <kozelkov.roman@gmail.com>
2017-10-01 15:48:48 +0300

    Initial commit

---
+++ src/imageSource.js
@@ -0,0 +1,12 @@
+function imageSourceFabric() {
+	var _this;
+	var currentIdx = 0;
+	return {
+		prev: function () {
+			return 'http://lorempixel.com/1000/700';
+		},
+		next: function () {
+			return 'http://lorempixel.com/1000/700';
+		}
+	}
+}
